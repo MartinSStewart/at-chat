6c5634374ee0d41c5f2e661ac96f6751800587b6
MartinSStewart <MartinSStewart@gmail.com>
2025-06-02 22:04:05 +0200

    derToJose function works

--- src/Vapid.elm
+++ src/Vapid.elm
@@ -1,4 +1,4 @@
-module Vapid exposing (generateRequestDetails)
+module Vapid exposing (bytesToHex, derToJose, encodeHex, generateRequestDetails)
 
 import Array
 import Bitwise
@@ -105,7 +105,7 @@ derToJose : Bytes -> Bytes
 derToJose signature =
     let
         paramBytes =
-            getParamSize 256
+            getParamSize 256 |> Debug.log "paramBytes"
 
         maxEncodedParamLength =
             paramBytes + 1
@@ -114,44 +114,159 @@ derToJose signature =
             Bytes.width signature
 
         seqLength =
-            if bytesGetAt 1 signature == Bitwise.or 1 maxOctet then
+            (if bytesGetAt 1 signature == Bitwise.or 1 maxOctet then
                 bytesGetAt 2 signature
 
-            else
+             else
                 bytesGetAt 1 signature
+            )
+                |> Debug.log "seqLength"
 
         offset0 =
-            if seqLength == Bitwise.or 1 maxOctet then
+            (if seqLength == Bitwise.or 1 maxOctet then
                 4
 
-            else
+             else
                 3
+            )
+                |> Debug.log "offset0"
 
         rLength =
-            bytesGetAt offset0 signature
+            bytesGetAt offset0 signature |> Debug.log "rLength"
 
         rOffset =
-            offset0 + 1
+            offset0 + 1 |> Debug.log "rOffset"
 
         offset1 =
-            offset0 + rLength + 2
+            offset0 + rLength + 2 |> Debug.log "offset1"
 
         sLength =
-            bytesGetAt offset1 signature
+            bytesGetAt offset1 signature |> Debug.log "sLength"
 
         sOffset =
-            offset1 + 1
+            offset1 + 1 |> Debug.log "sOffset"
 
         rPadding =
-            paramBytes - rLength
+            paramBytes - rLength |> Debug.log "rPadding"
 
         sPadding =
-            paramBytes - sLength
+            paramBytes - sLength |> Debug.log "sPadding"
 
         i =
-            offset1 + 1 + sLength
+            0
+
+        dst : Bytes
+        dst =
+            List.range 1 (rPadding + rLength + sPadding + sLength)
+                |> List.map (\_ -> Bytes.Encode.unsignedInt8 0)
+                |> Bytes.Encode.sequence
+                |> Bytes.Encode.encode
+
+        _ =
+            Debug.log "signature" (bytesToHex signature)
+
+        _ =
+            Debug.log "dst" (bytesToHex dst)
+
+        dst2 : Bytes
+        dst2 =
+            copy dst i signature (rOffset + max -rPadding 0) (rOffset + rLength)
+
+        _ =
+            Debug.log "dst2" (bytesToHex dst2)
+
+        offset =
+            paramBytes
+
+        dst3 =
+            copy dst2 offset signature (sOffset + max -sPadding 0) (sOffset + sLength)
+
+        --const dst = Buffer(rPadding + rLength + sPadding + sLength);
+        --
+        --var i = 0;//offset1 + 1 + sLength;
+        --for (i = 0; i < rPadding; i + 1) {
+        --	dst[offset] = 0;
+        --}
+        --
+        --signature.copy(dst, i, rOffset + Math.max(-rPadding, 0), rOffset + rLength);
+        --
+        --var offset = paramBytes;
+        --
+        --for (const o = offset; offset < o + sPadding; offset + 1) {
+        --	dst[offset] = 0;
+        --}
+        --signature.copy(dst, offset, sOffset + Math.max(-sPadding, 0), sOffset + sLength);
+        --
+        --console.log(dst.toString('hex'));
+        --return base64Url(dst.toString('base64'));
     in
-    0
+    dst3
+
+
+
+----seqLength: 69
+----paramBytes: 32
+----offset0: 3
+----rLength: 32
+----offset1: 37
+----sLength: 33
+----sPadding: -1
+----sOffset: 38
+----rPadding: 0
+----rOffset: 4
+--304502206591431e2e2c6645d6b75784f4429da8bd943bc6cbc1d1a2c76e74068d3be6c7022100f95b6ba3093f9cb37820873982425e78f3f15fcfa5db279cbe3b44ab07dd63f8
+--paramBytes 32
+--inputLength 71
+--seqLength 69
+--offset0 3
+--rLength 32
+--rOffset 4
+--offset1 37
+--sLength 33
+--sOffset 38
+--rPadding 0
+--sPadding -1
+--00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000
+--6591431e2e2c6645d6b75784f4429da8bd943bc6cbc1d1a2c76e74068d3be6c70000000000000000000000000000000000000000000000000000000000000000
+--6591431e2e2c6645d6b75784f4429da8bd943bc6cbc1d1a2c76e74068d3be6c7f95b6ba3093f9cb37820873982425e78f3f15fcfa5db279cbe3b44ab07dd63f8
+
+
+copy : Bytes -> Int -> Bytes -> Int -> Int -> Bytes
+copy target targetStart source sourceStart sourceEnd =
+    let
+        sourceSlice =
+            bytesSlice sourceStart sourceEnd source
+
+        a =
+            targetStart + Bytes.width sourceSlice |> Debug.log "a"
+    in
+    appendBytes
+        [ bytesSlice 0 targetStart target |> logBytes "target start"
+        , sourceSlice |> logBytes "sourceSlice"
+        , bytesSlice a (Bytes.width target) target |> logBytes "target end"
+        ]
+        |> logBytes "appended"
+        |> bytesSlice 0 (Bytes.width target)
+
+
+logBytes name bytes =
+    let
+        _ =
+            Debug.log name (bytesToHex bytes)
+    in
+    bytes
+
+
+bytesSlice : Int -> Int -> Bytes -> Bytes
+bytesSlice start end bytes =
+    Bytes.Decode.decode
+        (Bytes.Decode.map2
+            (\_ slice -> slice)
+            (Bytes.Decode.bytes start)
+            (Bytes.Decode.bytes (end - start))
+        )
+        bytes
+        |> Maybe.withDefault bytes
 
 
 bytesGetAt : Int -> Bytes -> Int
@@ -186,6 +301,7 @@ bytesToBase64 bytes =
     VendoredBase64.fromBytes bytes |> Maybe.withDefault ""
 
 
+encodeHex : List Int -> Bytes.Encode.Encoder
 encodeHex hexList =
     List.map
         Bytes.Encode.unsignedInt8
@@ -193,7 +309,7 @@ encodeHex hexList =
         |> Bytes.Encode.sequence
 
 
-bytesToHex : Bytes -> Maybe (Array.Array String)
+bytesToHex : Bytes -> Array.Array Int
 bytesToHex bytes2 =
     Bytes.Decode.decode
         (Bytes.Decode.loop
@@ -205,9 +321,10 @@ bytesToHex bytes2 =
                 else
                     Bytes.Decode.map
                         (\byte ->
-                            ( Array.push (Hex.toString byte) state, count - 1 ) |> Bytes.Decode.Loop
+                            ( Array.push byte state, count - 1 ) |> Bytes.Decode.Loop
                         )
                         Bytes.Decode.unsignedInt8
             )
         )
         bytes2
+        |> Maybe.withDefault Array.empty
--- tests/VapidTests.elm
+++ tests/VapidTests.elm
@@ -1,16 +1,44 @@
 module VapidTests exposing (..)
 
+import Array exposing (Array)
+import Bytes exposing (Bytes)
+import Bytes.Encode
 import Expect
+import Hex
+import List.Extra
 import Test exposing (Test)
-import Time
-import Unsafe
 import Vapid
+
+
+input : Bytes
+input =
+    String.toList "304502206591431e2e2c6645d6b75784f4429da8bd943bc6cbc1d1a2c76e74068d3be6c7022100f95b6ba3093f9cb37820873982425e78f3f15fcfa5db279cbe3b44ab07dd63f8"
+        |> List.Extra.greedyGroupsOf 2
+        |> List.filterMap (\chars -> String.fromList chars |> Hex.fromString |> Result.toMaybe)
+        |> Vapid.encodeHex
+        |> Bytes.Encode.encode
+
+
+expected : Array Int
+expected =
+    String.toList "6591431e2e2c6645d6b75784f4429da8bd943bc6cbc1d1a2c76e74068d3be6c7f95b6ba3093f9cb37820873982425e78f3f15fcfa5db279cbe3b44ab07dd63f8"
+        |> List.Extra.greedyGroupsOf 2
+        |> List.filterMap (\chars -> String.fromList chars |> Hex.fromString |> Result.toMaybe)
+        |> Array.fromList
 
 
 test : Test
 test =
     Test.only <|
-        Test.test "Vapid test" <|
+        Test.test "derToJose" <|
             \_ ->
-                Vapid.generateRequestDetails (Time.millisToPosix 0) (Unsafe.url "https://at-chat.app")
-                    |> Expect.equal "eyJ0eXAiOiJKV1QiLCJhbGciOiJFUzI1NiJ9"
+                Vapid.derToJose input
+                    |> Vapid.bytesToHex
+                    |> Expect.equal expected
+
+
+
+--Test.test "Vapid test" <|
+--    \_ ->
+--        Vapid.generateRequestDetails (Time.millisToPosix 0) (Unsafe.url "https://at-chat.app")
+--            |> Expect.equal "eyJ0eXAiOiJKV1QiLCJhbGciOiJFUzI1NiJ9"
