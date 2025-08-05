2a8e1e4102434f72d62de36cd8d3a9d8754ca312
Martin <martinsstewart@gmail.com>
2025-08-06 00:06:27 +0200

    wip

--- rust-server/src/main.rs
+++ rust-server/src/main.rs
@@ -3,20 +3,28 @@ use axum::{
     extract::Path,
     http::{StatusCode, Uri},
     routing::get,
+    routing::post,
+    body::{Bytes, Body},
 };
 use std::fs;
+use sha2::{Sha256, Digest};
 
 #[tokio::main]
 async fn main() {
     let app = Router::new()
-        .route("/file/{filename}", get(handler))
+        .route("/file/upload", post(upload_file_endpoint))
+        .route("/file/{filename}", get(get_file_endpoint))
         .fallback(fallback);
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
     axum::serve(listener, app).await.unwrap();
 }
 
-async fn handler(Path(path): Path<String>) -> (StatusCode, Vec<u8>) {
+async fn upload_file_endpoint(body: Bytes) -> (StatusCode, Vec<u8>) {
+    (StatusCode::BAD_REQUEST, b"Invalid filename".to_vec())
+}
+
+async fn get_file_endpoint(Path(path): Path<String>) -> (StatusCode, Vec<u8>) {
     let is_valid_filename = match path.split('.').collect::<Vec<_>>().as_slice() {
         [hash, filetype] => {
             hash.chars().all(|x| x.is_ascii_hexdigit())
