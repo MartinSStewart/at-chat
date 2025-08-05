9067a7d4989b115c0b61303c9131e3b151921174
Martin <martinsstewart@gmail.com>
2025-08-05 19:29:38 +0200

    Add nix

---
+++ rust-server/flake.nix
@@ -0,0 +1,47 @@
+{
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    naersk = {
+      url = "github:nix-community/naersk";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+  };
+
+  outputs =
+    {
+      self,
+      flake-utils,
+      naersk,
+      nixpkgs,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = (import nixpkgs) {
+          inherit system;
+        };
+
+        naersk' = pkgs.callPackage naersk { };
+
+        at-chat = naersk'.buildPackage {
+          src = ./.;
+        };
+      in
+      rec {
+        # For `nix build` & `nix run`:
+        packages = {
+          inherit at-chat;
+          default = at-chat;
+        };
+
+        # For `nix develop` (optional, can be skipped):
+        devShell = pkgs.mkShell {
+          nativeBuildInputs = with pkgs; [
+            rustc
+            cargo
+          ];
+        };
+      }
+    );
+}
