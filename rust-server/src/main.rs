5f441248cf8c966260e4b6651fdd1a27f072bebc
Martin <martinsstewart@gmail.com>
2025-08-05 15:35:50 +0200

    Add rust server

---
+++ rust-server/src/main.rs
@@ -0,0 +1,21 @@
+use axum::{
+    Router,
+    routing::get,
+    extract::Path,
+};
+
+#[tokio::main]
+async fn main() {
+    // build our application with a single route
+    let app = Router::new()
+        .route("/", get(|| async { "Hello, World!" }))
+        .route("/file/{filename}", get(handler));
+
+    // run our app with hyper, listening globally on port 3000
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
+}
+
+async fn handler(Path(path): Path<String>) -> String {
+    path
+}
