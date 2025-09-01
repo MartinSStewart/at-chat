4f8c145291ac5042d31086719f4b96166d5c694c
Martin <martinsstewart@gmail.com>
2025-09-01 12:09:59 +0200

    Override program-test v4

--- tests/EndToEndTests.elm
+++ tests/EndToEndTests.elm
@@ -19,7 +19,6 @@ import LoginForm
 import Pages.Home
 import Parser exposing ((|.), (|=))
 import PersonName
-import RecordedTests
 import SeqDict
 import Test.Html.Query
 import Test.Html.Selector
@@ -33,7 +32,7 @@ import Url exposing (Url)
 setup : T.ViewerWith (List (T.EndToEndTest ToBackend FrontendMsg FrontendModel ToFrontend BackendMsg BackendModel))
 setup =
     T.viewerWith tests
-        |> T.addBytesFiles (Dict.values RecordedTests.fileRequests)
+        |> T.addBytesFiles (Dict.values fileRequests)
 
 
 main : Program () (T.Model ToBackend FrontendMsg FrontendModel ToFrontend BackendMsg BackendModel) (T.Msg ToBackend FrontendMsg FrontendModel ToFrontend BackendMsg BackendModel)
@@ -41,9 +40,24 @@ main =
     T.startViewer setup
 
 
-homepageUrl : Url
-homepageUrl =
-    Unsafe.url "https://my-website.com"
+domain : Url
+domain =
+    { protocol = Url.Http, host = "localhost", port_ = Just 8000, path = "", query = Nothing, fragment = Nothing }
+
+
+{-| Please don't modify or rename this function
+-}
+fileRequests : Dict String String
+fileRequests =
+    [ ( "GET_http://localhost:3000/file/vapid", "/tests/data/1b846b6a39f0b828.txt" )
+    , ( "POST_https://api.postmarkapp.com/email", "/tests/data/2911db1dd6723eb4.txt" )
+    ]
+        |> Dict.fromList
+
+
+stringToJson : String -> Json.Encode.Value
+stringToJson json =
+    Result.withDefault Json.Encode.null (Json.Decode.decodeString Json.Decode.value json)
 
 
 handlePortToJs :
@@ -379,11 +393,38 @@ clickSpoiler user htmlId =
         ]
 
 
+handleRecordedHttpRequests : Dict String String -> Dict String Bytes -> { currentRequest : HttpRequest, data : T.Data FrontendModel BackendModel } -> HttpResponse
+handleRecordedHttpRequests overrides fileData { currentRequest } =
+    let
+        key : String
+        key =
+            currentRequest.method ++ "_" ++ currentRequest.url
+
+        getData : String -> HttpResponse
+        getData path =
+            case Dict.get path fileData of
+                Just data ->
+                    BytesHttpResponse { url = currentRequest.url, statusCode = 200, statusText = "OK", headers = Dict.empty } data
+
+                Nothing ->
+                    UnhandledHttpRequest
+    in
+    case ( Dict.get key overrides, Dict.get key fileRequests ) of
+        ( Just path, _ ) ->
+            getData path
+
+        ( Nothing, Just path ) ->
+            getData path
+
+        _ ->
+            UnhandledHttpRequest
+
+
 tests : Dict String Bytes -> List (T.EndToEndTest ToBackend FrontendMsg FrontendModel ToFrontend BackendMsg BackendModel)
 tests fileData =
     let
-        handleHttpRequests : ({ currentRequest : HttpRequest, data : T.Data FrontendModel BackendModel } -> Maybe HttpResponse) -> { currentRequest : HttpRequest, data : T.Data FrontendModel BackendModel } -> HttpResponse
-        handleHttpRequests overrides ({ currentRequest } as httpRequests) =
+        handleNormalHttpRequests : ({ currentRequest : HttpRequest, data : T.Data FrontendModel BackendModel } -> Maybe HttpResponse) -> { currentRequest : HttpRequest, data : T.Data FrontendModel BackendModel } -> HttpResponse
+        handleNormalHttpRequests overrides ({ currentRequest } as httpRequests) =
             case overrides httpRequests of
                 Just response ->
                     response
@@ -444,21 +485,31 @@ tests fileData =
         handleMultiFileUpload _ =
             UnhandledMultiFileUpload
 
-        config : T.Config ToBackend FrontendMsg FrontendModel ToFrontend BackendMsg BackendModel
+        normalConfig : T.Config ToBackend FrontendMsg FrontendModel ToFrontend BackendMsg BackendModel
+        normalConfig =
+            T.Config
+                Frontend.app_
+                Backend.app_
+                (handleNormalHttpRequests (\_ -> Nothing))
+                handlePortToJs
+                handleFileRequest
+                handleMultiFileUpload
+                domain
+
         config =
             T.Config
                 Frontend.app_
                 Backend.app_
-                (handleHttpRequests (\_ -> Nothing))
-                handlePortToJs
-                handleFileRequest
-                handleMultiFileUpload
-                homepageUrl
+                (handleRecordedHttpRequests Dict.empty fileData)
+                (\_ -> Nothing)
+                (\_ -> UnhandledFileUpload)
+                (\_ -> UnhandledMultiFileUpload)
+                domain
     in
     [ T.start
         "spoilers"
         startTime
-        config
+        normalConfig
         [ connectTwoUsers
             (\admin user ->
                 [ writeMessage admin "This message is ||very|| ||secret||"
@@ -490,7 +541,7 @@ tests fileData =
     , T.start
         "Notifications"
         startTime
-        config
+        normalConfig
         [ connectTwoUsers
             (\admin user ->
                 [ admin.input 100 (Dom.id "channel_textinput") "@Stevie Steve Hi!"
@@ -546,7 +597,7 @@ tests fileData =
     , T.start
         "Enable 2FA"
         startTime
-        config
+        normalConfig
         [ T.connectFrontend
             100
             sessionId0
@@ -648,7 +699,7 @@ tests fileData =
         ]
     , T.start "Logins are rate limited"
         startTime
-        config
+        normalConfig
         [ T.connectFrontend
             100
             sessionId0
@@ -742,7 +793,7 @@ tests fileData =
         ]
     , T.start "Test login"
         startTime
-        config
+        normalConfig
         [ T.connectFrontend
             100
             sessionId0
@@ -776,8 +827,37 @@ tests fileData =
             )
         , checkNoErrorLogs
         ]
+    , T.start
+        "new test"
+        (Time.millisToPosix 1756680301958)
+        normalConfig
+        [ T.connectFrontend
+            0
+            (Lamdera.sessionIdFromString "439199c04b8f7b594cdeedebc2a8029b82943b0a")
+            "/"
+            { width = 825, height = 1312 }
+            (\tab1 ->
+                [ tab1.portEvent 21 "check_notification_permission_from_js" (Json.Encode.string "granted")
+                , tab1.portEvent 2 "check_pwa_status_from_js" (stringToJson "false")
+                , tab1.portEvent 13 "is_push_subscription_registered_from_js" (stringToJson "true")
+                , tab1.portEvent 26 "load_user_settings_from_js" (Json.Encode.string "")
+                , tab1.click 3290 (Dom.id "homePage_loginButton")
+                , tab1.click 864 (Dom.id "please-add-an-id")
+                , tab1.input 1227 (Dom.id "loginForm_emailInput") "a@a.se"
+                , tab1.keyUp 933 (Dom.id "loginForm_emailInput") "Enter" []
+                , tab1.click 3544 (Dom.id "please-add-an-id")
+                , tab1.input 189 (Dom.id "loginForm_loginCodeInput") "22923193"
+                , tab1.input 3 (Dom.id "loginForm_loginCodeInput") "22923193"
+                , tab1.click 1384 (Dom.id "guild_openGuild_0")
+                , tab1.focus 19 (Dom.id "channel_textinput")
+                , tab1.click 941 (Dom.id "channel_textinput")
+                , tab1.input 468 (Dom.id "channel_textinput") "Test"
+                , tab1.blur 734 (Dom.id "channel_textinput")
+                , tab1.click 62 (Dom.id "messageMenu_channelInput_sendMessage")
+                ]
+            )
+        ]
     ]
-        ++ RecordedTests.tests fileData
 
 
 checkNoErrorLogs : T.Action toBackend frontendMsg frontendModel toFrontend backendMsg backendModel
