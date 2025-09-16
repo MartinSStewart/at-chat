a1d76866417331760ba9792ad686713ddca4fa7d
Martin <martinsstewart@gmail.com>
2025-09-16 22:51:08 +0200

    Fix 3

--- public/service-worker.js
+++ public/service-worker.js
@@ -28,7 +28,7 @@ self.addEventListener('fetch', (event) => {
     console.log(url);
 
     //const domain = 'https://at-chat.app';
-    const domain = 'https://https://at-chat.app/';
+    const domain = 'https://at-chat.app';
     if (url.startsWith(domain + '/file/t/')
         || url.startsWith(domain + '/file/0')
         || url.startsWith(domain + '/file/1')
