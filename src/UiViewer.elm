7817bf594acaf8d37fcb1b3ed853a69b859de973
MartinSStewart <MartinSStewart@gmail.com>
2026-02-06 16:54:28 +0100

    wip

--- src/UiViewer.elm
+++ src/UiViewer.elm
@@ -1,22 +1,32 @@
-module EmailViewer exposing (main)
+module UiViewer exposing (main)
+
+{-| This module is for letting you view parts of the app UI that would be difficult to reach under normal usage.
+Start `lamdera live` and go to localhost:8000/src/UiViewer.elm to use it.
+-}
 
 import Backend
 import Email.Html
 import Html exposing (Html)
 import Html.Attributes
 import String.Nonempty exposing (NonemptyString)
+import Ui
 
 
 main : Html msg
 main =
-    [ loginEmail ]
-        |> Html.div []
+    Ui.layout
+        []
+        (Ui.column
+            [ Ui.spacing 16 ]
+            [ Ui.html loginEmail
+            ]
+        )
 
 
 emailView : NonemptyString -> Email.Html.Html -> Html msg
 emailView subject content =
     Html.div
-        []
+        [ Html.Attributes.style "background-color" "white" ]
         [ Html.span []
             [ String.Nonempty.toString subject ++ " " |> Html.text
             , Html.input
