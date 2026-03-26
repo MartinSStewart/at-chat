b94d0597eef3d8c90f52ba1c6b54a049ed754a11
Martin Stewart <MartinSStewart@gmail.com>
2026-03-26 13:00:51 +0100

    Add test for starting Discord thread from message

--- tests/RecordedTests.elm
+++ tests/RecordedTests.elm
@@ -760,21 +760,21 @@ infoEndpointResponse =
     """{"s":"unknown","v":136,"h":["ce04ec5a052111b470b778b6adec9470dd0ab1d2","881990760d6345c8ebcecb11eeb3d7c3caa48d52","5bf58bad725a2b57b8b04c61329291b3ddc57f89","121b2b6733a1d45f0aa03a86227cb260fa0aca63","dc23f82c404f7f9881562c94f59dddf1f291d0b5","a7f4d07c436ed96853c669d38f8591f0d64d57cd"],"o":"a12","p":15}"""
 
 
-handleCustomRequest : String -> HttpResponse
-handleCustomRequest url =
+handleCustomRequest : String -> String -> HttpResponse
+handleCustomRequest method url =
     if String.startsWith "https://" url then
-        case String.dropLeft 8 url |> String.split "/" of
-            [ "discord.com", "api", "v9", "users", "@me" ] ->
+        case ( method, String.dropLeft 8 url |> String.split "/" ) of
+            ( "GET", [ "discord.com", "api", "v9", "users", "@me" ] ) ->
                 StringHttpResponse
                     { url = url, statusCode = 200, statusText = "OK", headers = Dict.empty }
                     """{"id":"184437096813953035","username":"at28727","avatar":"7c40cb63ea11096169c5a4dcb5825a3d","discriminator":"0","public_flags":0,"flags":0,"banner":null,"accent_color":null,"global_name":"AT2","avatar_decoration_data":null,"collectibles":null,"display_name_styles":null,"banner_color":null,"clan":null,"primary_guild":null,"mfa_enabled":false,"locale":"en-US","premium_type":0,"email":"a@a.se","verified":true,"phone":null,"nsfw_allowed":null,"linked_users":[],"bio":"","authenticator_types":[],"age_verification_status":1}"""
 
-            [ "discord.com", "api", "v9", "channels", _, "typing" ] ->
+            ( "POST", [ "discord.com", "api", "v9", "channels", _, "typing" ] ) ->
                 StringHttpResponse
                     { url = url, statusCode = 200, statusText = "OK", headers = Dict.empty }
                     ""
 
-            [ "discord.com", "api", "v9", "channels", channelId, "messages" ] ->
+            ( "POST", [ "discord.com", "api", "v9", "channels", channelId, "messages" ] ) ->
                 StringHttpResponse
                     { url = url, statusCode = 200, statusText = "OK", headers = Dict.empty }
                     ("""{
@@ -801,10 +801,13 @@ handleCustomRequest url =
 }"""
                     )
 
+            ( "PUT", [ "discord.com", "api", "v9", "channels", channelId, "thread-members", "@me" ] ) ->
+                StringHttpResponse { url = url, statusCode = 204, statusText = "OK", headers = Dict.empty } ""
+
             _ ->
                 let
                     _ =
-                        Debug.log "UnhandledHttpRequest" url
+                        Debug.log "UnhandledHttpRequest" ( method, url )
                 in
                 UnhandledHttpRequest
 
@@ -814,6 +817,22 @@ handleCustomRequest url =
                 Debug.log "UnhandledHttpRequest" url
         in
         UnhandledHttpRequest
+
+
+decodeCustomRequest request =
+    case request.body of
+        T.JsonBody json ->
+            Json.Decode.decodeValue
+                (Json.Decode.map2
+                    Tuple.pair
+                    (Json.Decode.field "method" Json.Decode.string)
+                    (Json.Decode.field "url" Json.Decode.string)
+                )
+                json
+                |> Result.toMaybe
+
+        _ ->
+            Nothing
 
 
 tests : Dict String Bytes -> String -> String -> Bytes -> String -> List (T.EndToEndTest ToBackend FrontendMsg FrontendModel ToFrontend BackendMsg BackendModel)
@@ -846,20 +865,15 @@ tests fileData discordOp0Ready discordOp0ReadySupplemental atUserIcon emojiJson 
                                 emojiJson
 
                         "http://localhost:3000/file/custom-request" ->
-                            case currentRequest.body of
-                                T.JsonBody json ->
-                                    case Json.Decode.decodeValue (Json.Decode.field "url" Json.Decode.string) json of
-                                        Ok url ->
-                                            handleCustomRequest url
+                            case decodeCustomRequest currentRequest of
+                                Just ( method, url ) ->
+                                    handleCustomRequest method url
 
-                                        Err error ->
-                                            let
-                                                _ =
-                                                    Debug.log "UnhandledHttpRequest" error
-                                            in
-                                            UnhandledHttpRequest
-
-                                _ ->
+                                Nothing ->
+                                    let
+                                        _ =
+                                            Debug.log "Failed to decode custom request" ()
+                                    in
                                     UnhandledHttpRequest
 
                         "http://localhost:3000/file/vapid" ->
@@ -1223,20 +1237,75 @@ tests fileData discordOp0Ready discordOp0ReadySupplemental atUserIcon emojiJson 
                     , user.input 100 Pages.Guild.channelTextInputId "Hello @purplelite!"
                     , user.keyDown 100 Pages.Guild.channelTextInputId "Enter" []
                     , user.checkView 100 (Test.Html.Query.has [ Test.Html.Selector.exactText "@purplelite" ])
+                    ]
+                )
+            ]
+        , T.start
+            "Other user starts thread from message"
+            startTime
+            normalConfig
+            [ linkDiscordAndLogin
+                sessionId0
+                (PersonName.toString Backend.adminUser.name)
+                adminEmail
+                False
+                discordOp0Ready
+                discordOp0ReadySupplemental
+                (\user ->
+                    [ user.click 100 (Dom.id "guild_openDiscordGuild_705745250815311942")
+                    , andThenWebsocket
+                        (\connection websocketState ->
+                            [ T.websocketSendString
+                                100
+                                connection
+                                "{\"t\":\"MESSAGE_CREATE\",\"s\":4,\"op\":0,\"d\":{\"type\":0,\"tts\":false,\"timestamp\":\"2026-03-26T10:56:15.024000+00:00\",\"pinned\":false,\"nonce\":\"1486680174970798080\",\"mentions\":[],\"mention_roles\":[],\"mention_everyone\":false,\"member\":{\"roles\":[],\"premium_since\":null,\"pending\":false,\"nick\":null,\"mute\":false,\"joined_at\":\"2020-05-01T11:39:39.915000+00:00\",\"flags\":0,\"deaf\":false,\"communication_disabled_until\":null,\"banner\":null,\"avatar\":null},\"id\":\"705745250815311942\",\"flags\":0,\"embeds\":[],\"edited_timestamp\":null,\"content\":\"Thread start message\",\"components\":[],\"channel_type\":0,\"channel_id\":\"1072828564317159465\",\"author\":{\"username\":\"at0232\",\"public_flags\":0,\"primary_guild\":null,\"id\":\"161098476632014848\",\"global_name\":\"AT\",\"display_name_styles\":null,\"discriminator\":\"0\",\"collectibles\":null,\"clan\":null,\"avatar_decoration_data\":null,\"avatar\":\"3d7b1aa7b5149fe06971b6dedf682d82\"},\"attachments\":[],\"guild_id\":\"705745250815311942\"}}"
+                            , T.websocketSendString
+                                100
+                                connection
+                                "{\"t\":\"MESSAGE_UPDATE\",\"s\":5,\"op\":0,\"d\":{\"type\":0,\"tts\":false,\"timestamp\":\"2026-03-26T10:56:15.024000+00:00\",\"pinned\":false,\"mentions\":[],\"mention_roles\":[],\"mention_everyone\":false,\"member\":{\"roles\":[],\"premium_since\":null,\"pending\":false,\"nick\":null,\"mute\":false,\"joined_at\":\"2020-05-01T11:39:39.915000+00:00\",\"flags\":0,\"deaf\":false,\"communication_disabled_until\":null,\"banner\":null,\"avatar\":null},\"id\":\"705745250815311942\",\"flags\":32,\"embeds\":[],\"edited_timestamp\":null,\"content\":\"Thread start message\",\"components\":[],\"channel_type\":0,\"channel_id\":\"1072828564317159465\",\"author\":{\"username\":\"at0232\",\"public_flags\":0,\"primary_guild\":null,\"id\":\"161098476632014848\",\"global_name\":\"AT\",\"display_name_styles\":null,\"discriminator\":\"0\",\"collectibles\":null,\"clan\":null,\"avatar_decoration_data\":null,\"avatar\":\"3d7b1aa7b5149fe06971b6dedf682d82\"},\"attachments\":[],\"guild_id\":\"705745250815311942\"}}"
+                            , T.websocketSendString
+                                100
+                                connection
+                                "{\"t\":\"GUILD_AUDIT_LOG_ENTRY_CREATE\",\"s\":6,\"op\":0,\"d\":{\"user_id\":\"161098476632014848\",\"target_id\":\"705745250815311942\",\"id\":\"1486680242226598131\",\"changes\":[{\"new_value\":\"Custom thread name\",\"key\":\"name\"},{\"new_value\":11,\"key\":\"type\"},{\"new_value\":false,\"key\":\"archived\"},{\"new_value\":false,\"key\":\"locked\"},{\"new_value\":4320,\"key\":\"auto_archive_duration\"},{\"new_value\":0,\"key\":\"rate_limit_per_user\"},{\"new_value\":0,\"key\":\"flags\"}],\"action_type\":110,\"guild_id\":\"705745250815311942\"}}"
+                            , T.websocketSendString
+                                100
+                                connection
+                                "{\"t\":\"THREAD_MEMBERS_UPDATE\",\"s\":7,\"op\":0,\"d\":{\"member_ids_preview\":[\"161098476632014848\",\"184437096813953035\"],\"member_count\":2,\"id\":\"705745250815311942\",\"added_members\":[{\"user_id\":\"184437096813953035\",\"presence\":{\"user\":{\"username\":\"at28727\",\"primary_guild\":null,\"id\":\"184437096813953035\",\"global_name\":\"AT2\",\"discriminator\":\"0\",\"clan\":null,\"bot\":false,\"avatar_decoration_data\":null,\"avatar\":\"7c40cb63ea11096169c5a4dcb5825a3d\"},\"status\":\"online\",\"processed_at_timestamp\":0,\"game\":null,\"client_status\":{\"web\":\"online\"},\"activities\":[]},\"muted\":false,\"mute_config\":null,\"member\":{\"user\":{\"username\":\"at28727\",\"public_flags\":0,\"primary_guild\":null,\"id\":\"184437096813953035\",\"global_name\":\"AT2\",\"display_name_styles\":null,\"display_name\":\"AT2\",\"discriminator\":\"0\",\"collectibles\":null,\"bot\":false,\"avatar_decoration_data\":null,\"avatar\":\"7c40cb63ea11096169c5a4dcb5825a3d\"},\"roles\":[],\"premium_since\":null,\"pending\":false,\"nick\":null,\"mute\":false,\"joined_at\":\"2025-10-11T19:44:51.312000+00:00\",\"flags\":0,\"deaf\":false,\"communication_disabled_until\":null,\"banner\":null,\"avatar\":null},\"join_timestamp\":\"2026-03-26T10:56:31.471913+00:00\",\"id\":\"705745250815311942\",\"flags\":1}],\"guild_id\":\"705745250815311942\"}}"
+                            , T.websocketSendString
+                                100
+                                connection
+                                "{\"t\":\"THREAD_CREATE\",\"s\":8,\"op\":0,\"d\":{\"type\":11,\"total_message_sent\":0,\"thread_metadata\":{\"locked\":false,\"create_timestamp\":\"2026-03-26T10:56:30.898915+00:00\",\"auto_archive_duration\":4320,\"archived\":false,\"archive_timestamp\":\"2026-03-26T10:56:30.898915+00:00\"},\"rate_limit_per_user\":0,\"parent_id\":\"1072828564317159465\",\"owner_id\":\"161098476632014848\",\"name\":\"Custom thread name\",\"message_count\":0,\"member_ids_preview\":[\"161098476632014848\",\"184437096813953035\"],\"member_count\":2,\"member\":{\"user_id\":\"184437096813953035\",\"muted\":false,\"mute_config\":null,\"join_timestamp\":\"2026-03-26T10:56:31.471913+00:00\",\"id\":\"705745250815311942\",\"flags\":1},\"last_message_id\":\"1486680242226598130\",\"id\":\"705745250815311942\",\"guild_id\":\"705745250815311942\",\"flags\":0}}"
+                            , T.andThen
+                                100
+                                (\data ->
+                                    case
+                                        List.filter
+                                            (\request ->
+                                                case ( Debug.log "url" request.url, decodeCustomRequest request ) of
+                                                    ( "http://localhost:3000/file/custom-request", Just ( method, url ) ) ->
+                                                        (Debug.log "url" url == "https://discord.com/api/v9/channels/705745250815311942/thread-members/@me")
+                                                            && (method == "PUT")
 
-                    --, user.custom
-                    --    100
-                    --    Pages.Guild.channelTextInputId
-                    --    "selectionchange"
-                    --    (Json.Encode.object
-                    --        [ ( "target"
-                    --          , Json.Encode.object
-                    --                [ ( "selectionStart", Json.Encode.int 7 )
-                    --                , ( "selectionEnd", Json.Encode.int 7 )
-                    --                ]
-                    --          )
-                    --        ]
-                    --    )
+                                                    _ ->
+                                                        False
+                                            )
+                                            (Debug.log "request" data.httpRequests)
+                                    of
+                                        [ _ ] ->
+                                            [ T.websocketSendString
+                                                100
+                                                connection
+                                                "{\"t\":\"MESSAGE_CREATE\",\"s\":9,\"op\":0,\"d\":{\"type\":0,\"tts\":false,\"timestamp\":\"2026-03-26T10:56:31.678000+00:00\",\"position\":0,\"pinned\":false,\"nonce\":\"1486680244629798912\",\"mentions\":[],\"mention_roles\":[],\"mention_everyone\":false,\"member\":{\"roles\":[],\"premium_since\":null,\"pending\":false,\"nick\":null,\"mute\":false,\"joined_at\":\"2020-05-01T11:39:39.915000+00:00\",\"flags\":0,\"deaf\":false,\"communication_disabled_until\":null,\"banner\":null,\"avatar\":null},\"id\":\"1486680245363802275\",\"flags\":0,\"embeds\":[],\"edited_timestamp\":null,\"content\":\"First message inside thread\",\"components\":[],\"channel_type\":11,\"channel_id\":\"705745250815311942\",\"author\":{\"username\":\"at0232\",\"public_flags\":0,\"primary_guild\":null,\"id\":\"161098476632014848\",\"global_name\":\"AT\",\"display_name_styles\":null,\"discriminator\":\"0\",\"collectibles\":null,\"clan\":null,\"avatar_decoration_data\":null,\"avatar\":\"3d7b1aa7b5149fe06971b6dedf682d82\"},\"attachments\":[],\"guild_id\":\"705745250815311942\"}}"
+                                            , user.checkView
+                                                100
+                                                (Test.Html.Query.has [ Test.Html.Selector.exactText "First message inside thread" ])
+                                            ]
+
+                                        _ ->
+                                            [ T.checkBackend 100 (\_ -> Err "Didn't join thread") ]
+                                )
+                            ]
+                        )
                     ]
                 )
             ]
