d5967538218786e5097cfe344dc3c88f10406812
Martin Stewart <MartinSStewart@gmail.com>
2026-08-03 00:38:37 +0200

    wip

--- src/MyUi.elm
+++ src/MyUi.elm
@@ -1098,7 +1098,7 @@ notoSansFontFaces =
         latinExtRange =
             "U+0100-02BA, U+02BD-02C5, U+02C7-02CC, U+02CE-02D7, U+02DD-02FF, U+0304, U+0308, U+0329, U+1D00-1DBF, U+1E00-1E9F, U+1EF2-1EFF, U+2020, U+20A0-20AB, U+20AD-20C0, U+2113, U+2C60-2C7F, U+A720-A7FF"
     in
-    fontFace "TrueType" "ascii" 400 "ascii.ttf" latinRange
+    fontFace "TrueType" "ascii" 400 "ascii.ttf" "U+0000-FFFF"
         :: List.map
             (\weight ->
                 fontFace "woff2" "Noto Sans" weight ("noto-sans-latin-" ++ String.fromInt weight ++ "-normal.woff2") latinRange
