94e045d5ca437e620dda361f9d2be3d1adfc39b2
Martin Stewart <MartinSStewart@gmail.com>
2026-09-07 09:50:24 +0200

    wip

--- src/MyUi.elm
+++ src/MyUi.elm
@@ -902,7 +902,7 @@ container topPadding isExpanded htmlId onPressedExpand backgroundColor isMobile2
 
                     else
                         16
-                , top = topPadding
+                , top = 12
                 , bottom = 0
                 }
             , Ui.row
